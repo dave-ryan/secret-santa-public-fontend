6b9fe0971feeceea7341acd461f5535ce06e5445
dave-a-ryan <daveryan145@gmail.com>
2022-02-07 17:02:33 -0600

    shows your christmas list

--- src/views/MyList.vue
+++ src/views/MyList.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>Hello from My LIST</div>
+  <div>
+    <div>
+      Here's your Christmas List!
+      <div v-for="item in myList" :key="item.id">
+        <br />
+        {{ item.name }} <br />
+        Link: {{ item.link }}
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      myList: {},
+    };
+  },
+  mounted() {
+    this.getMyList();
+  },
+  methods: {
+    getMyList: function () {
+      axios
+        .get("/wishedgifts")
+        .then((response) => {
+          this.myList = response.data;
+        })
+        .catch((errors) => {
+          console.log(errors);
+        });
+    },
+  },
+};
+</script>
