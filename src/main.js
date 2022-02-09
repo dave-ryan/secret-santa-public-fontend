14a444ed7af44db57fc0d778dc187c9d0b3a9005
dave-a-ryan <daveryan145@gmail.com>
2022-02-09 15:23:44 -0600

    installed bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
