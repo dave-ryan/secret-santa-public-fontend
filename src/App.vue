d2872e62f6a31abf1869936183a77a543e5df2af
dave-a-ryan <daveryan145@gmail.com>
2022-02-10 14:12:48 -0600

    removing app css, will replace with other css later

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,15 @@
 <template>
   <div id="nav" v-if="loginStatus">
+    <h2>Griffith</h2>
+
     <router-link to="/">Home</router-link> |
     <router-link to="/my-list">My List</router-link> |
     <router-link to="/login" @click="logOut()">Log Out</router-link>
   </div>
-
   <router-view @updateParent="updateLogin"></router-view>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
 
 <script>
