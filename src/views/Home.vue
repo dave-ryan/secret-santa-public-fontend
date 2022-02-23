5e3327d7f1f5e3738525ef7b9c2294b0ae8ace5b
dave-a-ryan <daveryan145@gmail.com>
2022-02-23 17:55:15 -0600

    proper column formatting and fixed the checkmarks not aligning center

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="container" id="home">
-    <div class="col-12 align-items-center vh-100">
-      <div class="row">
-        <div class="col-12">
-          Your family:
-          <div v-for="user in family" :key="user.id">
-            <br />
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        Your family:
+        <div class="row mt-3" v-for="user in family" :key="user.id">
+          <div class="col-12-">
             <button
               class="btn btn-outline-success"
               data-bs-toggle="collapse"
@@ -19,6 +18,8 @@
               aria-expanded="false"
               :id="`christmas-list-${user.id}`"
             >
+              (sample item)
+
               <div v-for="item in christmasLists[`${user.id}`]" :key="item.id">
                 <div v-if="item.purchaser_id">
                   <span class="text-decoration-line-through">{{
@@ -28,42 +29,46 @@
                     (bought by: {{ item.purchaser.name }})</span
                   >
                 </div>
-                <span v-else>{{ item.name }}</span>
+                <div v-else>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" value="" />
+                    <label class="form-check-label" for="flexCheckDefault">
+                      {{ item.name }}
+                    </label>
+                  </div>
+                </div>
               </div>
-              (sample item)
             </div>
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="row">
-        <div class="col-12">
-          <hr />
-          Your secret santa:
-          <br />
-          <button
-            class="btn btn-outline-success"
-            data-bs-toggle="collapse"
-            data-bs-target="#christmas-list-ss"
+    <div class="row">
+      <div class="col-12">
+        <hr />
+        Your secret santa:
+        <br />
+        <button
+          class="btn btn-outline-success"
+          data-bs-toggle="collapse"
+          data-bs-target="#christmas-list-ss"
+        >
+          {{ secretSanta.name }}
+        </button>
+        <div class="collapse" aria-expanded="false" id="christmas-list-ss">
+          <div
+            v-for="item in christmasLists[`${secretSanta.id}`]"
+            :key="item.id"
           >
-            {{ secretSanta.name }}
-          </button>
-          <div class="collapse" aria-expanded="false" id="christmas-list-ss">
-            <div
-              v-for="item in christmasLists[`${secretSanta.id}`]"
-              :key="item.id"
-            >
-              {{ item.name }}
-            </div>
-            (sample item)
+            {{ item.name }}
           </div>
+          (sample item)
         </div>
       </div>
     </div>
   </div>
 </template>
-
-
 
 <script>
 import axios from "axios";
